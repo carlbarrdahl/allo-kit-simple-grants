59dcb26673a4ce3f249ebd1034e9b044e8c39ce3
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-10 20:04:36 +0100

    Fix indexer config

--- packages/app/lib/graphql.ts
+++ packages/app/lib/graphql.ts
@@ -3,6 +3,7 @@ import { Client, cacheExchange, fetchExchange } from "urql";
 const urls = {
   [31337]:
     process.env.NEXT_PUBLIC_INDEXER_URL_31337 || "http://localhost:42069",
+  [84532]: process.env.NEXT_PUBLIC_INDEXER_URL_84532,
 };
 export function createClient(chainId: number) {
   const url = urls[chainId as keyof typeof urls];
